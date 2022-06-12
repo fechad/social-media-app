40df503c3cfd6156b5bd0c72a1224cef081c7712
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-06-12 00:09:12 -0400

    fixed warnings

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import '../styles/Button.css'
 
 const Button = ({color, icon, text, state} : {color: string, icon: string, text: string, state: string}) => {
   return (
-    <div style={ state == 'disabled' ? {backgroundColor: '#D9D9D9'} : {backgroundColor: 
+    <div style={ state === 'disabled' ? {backgroundColor: '#D9D9D9'} : {backgroundColor: 
         color}} className='ButtonComponent'>
-        <p style={state == 'disabled' ?{color: 'darkgrey'} : {color: 'white'}}>{text}</p>
+        <p style={state === 'disabled' ?{color: 'darkgrey'} : {color: 'white'}}>{text}</p>
     </div>
   )
 }
