0d4b2e066f1f141a64253afaa2a5d6e1551a8a02
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-06-17 19:49:07 -0400

    finished checkbox

--- client/src/components/Checkbox.tsx
+++ client/src/components/Checkbox.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import '../styles/Checkbox.css'
 import Text from '../components/Text';
+import { FaCheck } from 'react-icons/fa';
+import { useState } from 'react';
+
+
 
 const Checkbox = ({text}:{text: string}) => {
+
+  const [checked, setCheck] = useState(false);
+
   return (
-    <div className='Checkbox'>
-        <input type="checkbox" />
+    <div className='CheckboxContainer'>
+        <div className={`Checkbox ${checked ? 'Checked' : ''}`} onClick={() => setCheck(!checked)}><FaCheck color='white'/></div>
         <Text content={text}/>
     </div>
   )
