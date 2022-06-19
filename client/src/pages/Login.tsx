f49d13ade463514091fe4d892474c55cb5d5d7a4
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-06-19 16:27:58 -0400

    Login page done just maybe small css enhancements

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,30 +4,35 @@ import Link from '../components/Link';
 import Text from '../components/Text';
 import TextInput from '../components/TextInput';
 import { FcGoogle} from "react-icons/fc";
+import {IoArrowBack} from "react-icons/io5"
+import {BsArrowLeft} from "react-icons/bs"
 import '../styles/Login.css'
 
 const Login = () => {
   return (
-    <div className='LoginContainer'>
-        <section className='LoginHeader'>
-            <img src='logo.svg' alt = ' test' height="87"width="50"></img>
-            <Text type='H3' content='Name of app'/>
-        </section>
-        <TextInput label='Username' />
-        <section className='PasswordSection'>
-            <Link underlined={false} content='Forgot password?'/>
-            <TextInput type='password' />
-        </section>
-        <section className='Separator'>
-            <hr />
-            <Text type='H2' content='OR'/>
-            <hr />
-        </section>
-        <Button text='Sign in with Google' icon={<FcGoogle />} url='https://www.google.com/?gws_rd=ssl'/>
-        <section className='SignUpSection'>
-            <Text type='H3' content="Don't have an account?"/>
-            <Link underlined={true} content='Sign-up'/>
-        </section>
+    <div className='LoginPage'>
+        <a className='ArrowBack' href='/'><BsArrowLeft size={40}/></a>
+        <div className='LoginContainer'>
+            <section className='LoginHeader'>
+                <img src='logo.svg' alt = ' test' height="87"width="50"></img>
+                <Text type='H3' content='Name of app'/>
+            </section>
+            <TextInput label='Username' />
+            <section className='PasswordSection'>
+                <Link underlined={false} content='Forgot password?'/>
+                <TextInput type='password' />
+            </section>
+            <section className='Separator'>
+                <hr />
+                <Text type='H2' content='OR'/>
+                <hr />
+            </section>
+            <Button text='Sign in with Google' icon={<FcGoogle />} url='https://www.google.com/?gws_rd=ssl'/>
+            <section className='SignUpSection'>
+                <Text type='H3' content="Don't have an account?"/>
+                <Link underlined={true} content='Sign-up'/>
+            </section>
+        </div>
     </div>
   )
 }
