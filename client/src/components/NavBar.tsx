87f0e1917beb219d14c6e9ef8c12a8e21e13d435
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-07-15 23:32:42 -0400

    started nav bar component and pages set-up

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/NavBar.scss'
+
+const NavBar = () => {
+  return (
+    <div className='NavBarContainer'>
+        <div >
+
+        </div>
+
+    </div>
+  )
+}
+
+export default NavBar
