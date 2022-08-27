f6028274649789e128462912d8005ac4718499fc
Fedwin Chatelier <71038300+fechad@users.noreply.github.com>
2022-08-26 22:22:20 -0400

    done with page for now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import './App.scss';
-import Text from '../src/components/Text';
 import Link from './components/Link';
 import { BiCopyright } from 'react-icons/bi';
 import Button from './components/Button';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useNavigate } from 'react-router-dom';
 
 
 
 function App() {
+  let navigate = useNavigate();
+  AOS.init({once: false, mirror: true})
   return (
     <div className='App'>
       <section className='page-header'>
         <section className='nav-bar'>
-          <img src='/logo.svg' alt="" height="80px"width="80px"></img>
+          <img src='/logo.svg' alt="" height="80px"width="80px" onClick={() => document.getElementsByClassName('app-summary')[0].scrollIntoView({block: 'center', inline: 'center'})}></img>
           <div className='nav-bar-sections'>
-            <p>About the app</p>
-            <p>About the team</p>
-            <p>Help</p>
+            <p onClick={() => document.getElementsByClassName('app-sample')[0].scrollIntoView({block: 'center', inline: 'center'})}>About the app</p>
+            <p onClick={() => document.getElementsByClassName('team-bios')[0].scrollIntoView({block: 'nearest', inline: 'center'})}>About the team</p>
+            <p></p>
           </div>
           <Link content='Login' underlined={true} url='/login'/>
         </section>
       </section>
       <section className='app-summary'>
-        <div className='app-modal'>
+        <div className='app-modal' 
+          data-aos='slide-down'
+          data-aos-duration="1500"
+          data-aos-once="false"
+        >
           <div className='app-modal-header'>
             <p style={{fontSize: '60px', fontWeight: 'Bold'}}>Welcome to Chymera</p>
           </div>
@@ -30,17 +38,27 @@ function App() {
             <p style={{fontSize: '35px'}}>This is a social media website project designed to allow users to share their opinions and moments, interact with others through chats and get informed on the latest news updates.</p>
           </div>
           <div className='app-modal-footer'>
-            <Button color='#5CE1E6' text='Login' textType='H2'/>
-            <Button text='Sign up' textType='H2'/>
+            <Button color='#5CE1E6' text='Login' textType='H2' fct={() => navigate('/login', {replace: true})}/>
+            <Button text='Sign up' textType='H2'fct={() => navigate('/Sign-up', {replace: true})}/>
           </div>
         </div>
-        <img className='summary-logo' src='/logo.svg' alt="" height="288px"width="288px"></img>
+        <div className='summary-logo-circle'>
+          <img className='summary-logo' src='/logo.svg' alt="" height="188px"width="188px" data-aos='fade' data-aos-duration="1500" data-aos-once="false"></img>
+        </div>
       </section>
       <section className='app-sample'>
-        <div className='sample-image'>
+        <div className='sample-image'
+          data-aos='slide-right'
+          data-aos-duration="1500"
+          data-aos-once="false"
+        >
           <img src='/create_and_share.jpg' alt="" height="400"width="528px"></img>
         </div>
-        <div className='sample-description'>
+        <div className='sample-description'
+          data-aos='fade-right'
+          data-aos-duration="1500"
+          data-aos-once="false"
+        >
           <div className='sample-description-header'>
             <p style={{fontSize: '50px', fontWeight: 'Bold'}}>Create and Share</p>
           </div>
@@ -50,7 +68,11 @@ function App() {
         </div>
       </section>
       <section className='app-sample'>
-        <div className='sample-description-reverse'>
+        <div className='sample-description-reverse'
+          data-aos='fade-left'
+          data-aos-duration="1500"
+          data-aos-once="false"
+        >
           <div className='sample-description-header'>
             <p style={{fontSize: '50px', fontWeight: 'Bold'}}>Connect and Discuss</p>
           </div>
@@ -58,15 +80,27 @@ function App() {
             <p style={{fontSize: '35px'}}>Chat with friends and loved ones thanks to our chat feature. Weather it’s in single conversations or chat groups, Chymera makes it easy to communicate.</p>
           </div>
         </div>
-        <div className='sample-image'>
+        <div className='sample-image'
+          data-aos='slide-left'
+          data-aos-duration="1500"
+          data-aos-once="false"
+        >
           <img src='/connect_and_discuss.jpg' alt="" height="400"width="528px"></img>
         </div>
       </section>
       <section className='app-sample'>
-        <div className='sample-image'>
+        <div className='sample-image'
+          data-aos='slide-right'
+          data-aos-duration="1500"
+          data-aos-once="false"
+        >
           <img src='/stay_informed.jpg' alt="" height="400"width="528px"></img>
         </div>
-        <div className='sample-description'>
+        <div className='sample-description'
+          data-aos='fade-right'
+          data-aos-duration="1500"
+          data-aos-once="false"
+        >
           <div className='sample-description-header'>
             <p style={{fontSize: '60px', fontWeight: 'Bold'}}>Stay Informed</p>
           </div>
@@ -76,42 +110,52 @@ function App() {
         </div>
       </section>
       <section className='team-bios'>
-        <div className='team-bios-title'>
-          <p>About the team</p>
-        </div>
-        <div className='team-bios-content'>
-          <div className='team-member'>
-            <div className='team-member-photo-container'>
-              <img className='team-member-photo' src='/fedwin.jpg' alt="" height="288px"width="288px"></img>
-            </div>
-            <div className='team-member-name'>
-              <p>Fedwin Chatelier</p>
-            </div>
-            <div className='team-member-description'>
-              <p>
-                Hi my name is Fedwin, I’m currently a software engenieering student at Polytechnique of Montreal. In this project I played the roles of Web Architech and FullStack software developper. 
-              </p>
-            </div>
-            <div className='team-member-contact-info'>
-              <p>
-                LinkedIn: {<a href='https://www.linkedin.com/in/fedwin-chatelier-52282a230/'>Fedwin Chatelier</a>}
-                <br/>GitHub:{<a href='https://github.com/fechad'>fechad</a>}
-                <br/>Buisiness email: oveezion@gmail.com
-              </p>
-            </div>
+        <div data-aos='slide-up' data-aos-duration="1000" data-aos-once='false'>
+          <div className='team-bios-title'>
+            <p>Creators</p>
           </div>
-          <div className='team-member-2'>
-            <div className='team-member-photo-container'>
-              <img className='team-member-photo' src='/fedwin.jpg' alt="" height="288px"width="288px"></img>
+          <div className='team-bios-content'>
+            <div className='team-member'
+              data-aos='fade-right'
+              data-aos-duration="1500"
+              data-aos-once="false"
+            >
+              <div className='team-member-photo-container'>
+                <img className='team-member-photo' src='/fedwin.jpg' alt="" height="288px"width="288px"></img>
+              </div>
+              <div className='team-member-name'>
+                <p>Fedwin Chatelier</p>
+              </div>
+              <div className='team-member-description'>
+                <p>
+                  Hi my name is Fedwin, I’m currently a software engenieering student at Polytechnique of Montreal. In this project I played the roles of Web Architech and FullStack software developper. 
+                </p>
+              </div>
+              <div className='team-member-contact-info'>
+                <p>
+                  LinkedIn: {<a href='https://www.linkedin.com/in/fedwin-chatelier-52282a230/'>Fedwin Chatelier</a>}
+                  <br/>GitHub:{<a href='https://github.com/fechad'>fechad</a>}
+                  <br/>Buisiness email: oveezion@gmail.com
+                </p>
+              </div>
             </div>
-            <div className='team-member-name'>
-              <p>Étienne Aumais Boucher</p>
-            </div>
-            <div className='team-member-description'>
+            <div className='team-member-2'
+              data-aos='fade-left'
+              data-aos-duration="1500"
+              data-aos-once="false"
+            >
+              <div className='team-member-photo-container'>
+                <img className='team-member-photo' src='/etienne.jpg' alt="" height="288px"width="288px"></img>
+              </div>
+              <div className='team-member-name'>
+                <p>Étienne Aumais Boucher</p>
+              </div>
+              <div className='team-member-description'>
 
-            </div>
-            <div className='team-member-contact-info'>
+              </div>
+              <div className='team-member-contact-info'>
 
+              </div>
             </div>
           </div>
         </div>
