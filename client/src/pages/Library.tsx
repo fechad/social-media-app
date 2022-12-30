c6bb435c0daf3eb39a0bbe6411b8ee302c4bc58d
Etienne123456 <etienneaumais@gmail.com>
2022-12-29 23:23:47 -0500

    remove eslint error

--- client/src/pages/Library.tsx
+++ client/src/pages/Library.tsx
@@ -14,6 +14,7 @@ import ChatPreview from '../components/ChatPreview';
 import LeftSidePane from '../components/LeftSidePane';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import RightSidePane from '../components/RightSidePane';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import NavBar from '../components/NavBar';
 import RadioButton from '../components/RadioButton';
 import Modal from '../components/Modal';
